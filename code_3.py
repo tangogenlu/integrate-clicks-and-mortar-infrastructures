fad4025ee8009f870af44915ef8dc13c44378cbf
Геннадий <140153539+tangogenlu@users.noreply.github.com>
2024-02-01 11:02:17 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,17 +1,13 @@
-}
-}
-
-main();
-    return data;
-        data.push(Math.floor(Math.random() * 100) + 1);
-function main() {
-    for (let i = 0; i < 10; i++) {
-
+        }
     }
-    data.forEach(item => {
-function generateRandomData() {
-        console.log("Random Number: " + item);
-    });
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
 
-    const data = [];
-    const data = generateRandomData();
+        for (int item : data) {
+
+}
+public class RandomDataGenerator {
